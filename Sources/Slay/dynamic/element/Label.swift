6ae6bac358a01b4f67a8566ad8bafe3f89c6e29d
RandomHashTags <imrandomhashtags@gmail.com>
2025-11-18 10:37:54 -0600

    make `image` optional for `Label`

--- Sources/Slay/dynamic/element/Label.swift
+++ Sources/Slay/dynamic/element/Label.swift
@@ -1,11 +1,11 @@
 
 public struct Label {
-    public var image:Image
+    public var image:Image?
     public var text:Text
 
     public init(
         _ text: Text,
-        image: Image
+        image: Image?
     ) {
         self.text = text
         self.image = image
@@ -15,10 +15,10 @@ public struct Label {
 // MARK: Layoutable
 extension Label: Layoutable {
     public var width: Int32 {
-        image.width + text.width
+        (image?.width ?? 0) + text.width
     }
 
     public var height: Int32 {
-        min(image.height, text.height)
+        min(image?.height ?? 0, text.height)
     }
 }
