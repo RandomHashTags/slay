bdd3ad390702e5f7b2fff01187eeba9576ae6f9c
RandomHashTags <imrandomhashtags@gmail.com>
2025-11-18 09:59:00 -0600

    add `Toggle`

---
+++ Sources/Slay/dynamic/Toggle.swift
@@ -0,0 +1,27 @@
+
+public struct Toggle {
+    public var text:Text
+    public var image:Image?
+    public var isOn:Bool
+
+    public init(
+        _ text: Text,
+        image: Image? = nil,
+        isOn: Bool
+    ) {
+        self.text = text
+        self.image = image
+        self.isOn = isOn
+    }
+}
+
+// MARK: Layoutable
+extension Toggle: Layoutable {
+    public var width: Int32 {
+        text.width + (image?.width ?? 0) // TODO: what should the toggle switch width be?
+    }
+
+    public var height: Int32 {
+        max(text.height, image?.height ?? 0) // TODO: what should the toggle switch height be?
+    }
+}
