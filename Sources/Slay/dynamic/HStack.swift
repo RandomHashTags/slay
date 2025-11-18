8a4e908d46d2b23c932e89ec9290bb85b9b10e8e
RandomHashTags <imrandomhashtags@gmail.com>
2025-11-18 10:11:08 -0600

    fix: `HStack`'s `height` calculation

--- Sources/Slay/dynamic/HStack.swift
+++ Sources/Slay/dynamic/HStack.swift
@@ -14,6 +14,6 @@ extension HStack: Layoutable {
     }
 
     public var height: Int32 {
-        data.min(by: { $0.height < $1.height })?.height ?? 0
+        data.max(by: { $0.height < $1.height })?.height ?? 0
     }
 }
