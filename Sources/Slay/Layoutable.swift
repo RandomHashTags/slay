cd3573e34b0cca80555bebc3020168230bf7c894
RandomHashTags <imrandomhashtags@gmail.com>
2025-11-18 09:41:42 -0600

    add documentation to `Layoutable` variables

--- Sources/Slay/Layoutable.swift
+++ Sources/Slay/Layoutable.swift
@@ -1,5 +1,8 @@
 
 public protocol Layoutable: Sendable {
+    /// Number of horizontal pixels this layoutable uses.
     var width: Int32 { get }
+
+    /// Number of vertical pixels this layoutable uses.
     var height: Int32 { get }
 }
