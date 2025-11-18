f092fd74eb2cb990f37d20079a870947bb5c2ab6
RandomHashTags <imrandomhashtags@gmail.com>
2025-11-18 11:22:53 -0600

    `HStack` improvements & unit test updates; added `VStackTests`

--- Sources/Slay/dynamic/shape/Rectangle.swift
+++ Sources/Slay/dynamic/shape/Rectangle.swift
@@ -1,16 +1,32 @@
 
 public struct Rectangle {
+    package var _width:Int32
+    package var _height:Int32
+
     public init() {
+        _width = 0
+        _height = 0
+    }
+    public init(width: Int32, height: Int32) {
+        _width = width
+        _height = height
+    }
+}
+
+extension Rectangle {
+    public mutating func frame(width: Int32, height: Int32) {
+        _width = width
+        _height = height
     }
 }
 
 // MARK: View
 extension Rectangle: View {
     public var width: Int32 {
-        0 // TODO: fix
+        _width
     }
 
     public var height: Int32 {
-        0 // TODO: fix
+        _height
     }
 }
--- Tests/SlayTests/VStackTests.swift
+++ Tests/SlayTests/VStackTests.swift
@@ -3,17 +3,17 @@ import Slay
 import Testing
 
 @Suite
-struct HStackTests {
+struct VStackTests {
 
     @Test
-    func hstackFrameCalculation() {
-        let hstack = HStack([
+    func vstackFrameCalculation() {
+        let vstack = VStack([
             Rectangle(width: 10, height: 10),
             Rectangle(width: 20, height: 20),
             Rectangle(width: 13, height: 15),
             Rectangle(width: 7, height: 2)
         ])
-        #expect(hstack.width == 50)
-        #expect(hstack.height == 20)
+        #expect(vstack.width == 20)
+        #expect(vstack.height == 47)
     }
 }
