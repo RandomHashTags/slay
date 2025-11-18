2d7316f3ecf9cd3a72a673abb6c07d4d024c8fe9
RandomHashTags <imrandomhashtags@gmail.com>
2025-11-18 10:03:40 -0600

    add `Rectangle` and `Circle`

---
+++ Sources/Slay/dynamic/shape/Circle.swift
@@ -0,0 +1,16 @@
+
+public struct Circle {
+    public init() {
+    }
+}
+
+// MARK: Layoutable
+extension Circle: Layoutable {
+    public var width: Int32 {
+        0 // TODO: fix
+    }
+
+    public var height: Int32 {
+        0 // TODO: fix
+    }
+}
---
+++ Sources/Slay/dynamic/shape/Rectangle.swift
@@ -0,0 +1,16 @@
+
+public struct Rectangle {
+    public init() {
+    }
+}
+
+// MARK: Layoutable
+extension Rectangle: Layoutable {
+    public var width: Int32 {
+        0 // TODO: fix
+    }
+
+    public var height: Int32 {
+        0 // TODO: fix
+    }
+}
